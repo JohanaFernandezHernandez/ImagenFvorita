3e20c17d4c35c619d85f8af228a01d8cdc5cf49e
Johana Fernandez <najofe5@gmail.com>
2025-02-08 08:31:52 +0100

    feat: Conexion de Back con el Front

---
+++ Front/src/hooks/useConextionApi.jsx
@@ -0,0 +1,82 @@
+import axios from "axios";
+const url = import.meta.env.VITE_API_URL;
+
+export const useConectionApi = () => {
+
+    //Obtener Imagenes
+    const getImages = async() => {
+        try {
+            const response = await axios.get(`${url}/getImagen`);
+            console.log(response);
+            if (response.status === 200) {
+              return response.data;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+    };
+
+
+    //obtener una imagen por su id
+    const getImageById = async(id) => {
+        try {
+            const response = await axios.get(`${url}/getImagenById/${id}`);
+            if (response.status === 200) {
+              return response.data;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+    };
+
+
+    //Agregar una nueva imagen
+    const createImage = async () => {
+        try {
+            const response = await axios.post(`${url}/createImagen`, { Title, ImagenURL });
+            if (response.status === 201) {
+              return response.data;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+    };
+
+    //Actualizar una imagen
+    const updateImage = async (id, Title, ImagenURL) => {
+        try {
+            const response = await axios.put(`${url}/updateImagen/${id}`, { Title, ImagenURL });
+            if (response.status === 200) {
+              return response.data;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+    };
+
+    //Borrar una imagen
+    const deleteImage = async (id) => {
+        try {
+            const response = await axios.delete(`${url}/deleteImagen/${id}`);
+            if (response.status === 200) {
+              return response.data;
+            }
+          } catch (error) {
+            console.log(error);
+          }
+    };
+
+
+
+
+
+
+    return {
+        getImages,
+        getImageById,
+        createImage,
+        updateImage,
+        deleteImage,
+      };
+
+}
--- Front/src/pages/HomePage/HomePage.jsx
+++ Front/src/pages/HomePage/HomePage.jsx
@@ -2,22 +2,30 @@ import { Card, Header } from "../../Components";
 import "./HomePage.css";
 import Flor from "../../assets/Flor.jpg";
 import Flor2 from "../../assets/Flor2.avif";
+import { useConectionApi } from "../../hooks/useConextionApi";
+import { useEffect, useState } from "react";
 
 export const HomePage = () => {
+  const[images, setImages] = useState([]);
+
+  const {getImages} = useConectionApi();
+
+  useEffect(() => {
+    const fetchImage = async ()=> {
+      const response = await getImages();
+      setImages(response);
+    }
+    fetchImage();
+
+  },[]);
+
+  console.log(images);
+
+
   return (
     <section className="container-card">
       <Card img={Flor} title="Hermosa Flor" />
       <Card img={Flor2} title="Hermosa Flor" />
-
-      <Card img={Flor} title="Hermosa Flor" />
-
-      <Card img={Flor} title="Hermosa Flor" />
-
-      <Card img={Flor2} title="Hermosa Flor" />
-
-      <Card img={Flor} title="Hermosa Flor" />
-
-      <Card img={Flor} title="Hermosa Flor" />
 
       <Card img={Flor} title="Hermosa Flor" />
 
