3a1b2f7104ecf5ef064aa9998d34e8f19e1a4c56
Johana Fernandez <najofe5@gmail.com>
2025-02-11 11:15:34 +0100

    feat: Ajustes finales estilos card

--- Front/src/Components/Cards/Card.jsx
+++ Front/src/Components/Cards/Card.jsx
@@ -1,4 +1,14 @@
-import { Button, Box, Modal, TextField, Typography, Card as MuiCard, CardContent, CardActions, CardMedia } from "@mui/material";
+import {
+  Button,
+  Box,
+  Modal,
+  TextField,
+  Typography,
+  Card as MuiCard,
+  CardContent,
+  CardActions,
+  CardMedia,
+} from "@mui/material";
 import Borrar from "../../assets/svg/Borrar";
 import Editar from "../../assets/svg/Editar";
 import { useState, useEffect } from "react";
@@ -56,128 +66,163 @@ export const Card = ({ img, title, id, onDelete, onEdit }) => {
 
   return (
     <MuiCard
-  sx={{
-    width: 350,
-    borderRadius: 3,
-    boxShadow: "0px 4px 8px rgba(255, 255, 255, 0.1)", // Sombra sutil en gris/blanco
-    transition: "transform 0.3s ease, box-shadow 0.3s ease", // Animación suave en hover
-    backgroundColor: "#1e1e1e", // Fondo oscuro para la card
-    "&:hover": {
-      boxShadow: "0px 8px 16px rgba(255, 255, 255, 0.2)", // Sombra más intensa en hover
-      transform: "scale(1.03)", // Efecto de zoom sutil
-    },
-    overflow: "hidden",
-  }}
->
-  {/* Imagen */}
-  <CardMedia
-    component="img"
-    height="220"
-    image={img}
-    alt={title}
-    sx={{
-      objectFit: "cover",
-      objectPosition: "top",
-      borderBottom: "1px solid rgba(255, 255, 255, 0.1)", // Línea separadora sutil
-    }}
-  />
-
-  {/* Contenido */}
-  <CardContent sx={{ padding: "10px" }}>
-    <Typography variant="p" fontWeight="bold" textAlign="center" color="#ffffff" sx={{
-       fontWeight: "400",
-       fontSize: {
-         xs: "16px",
-         md: "18px",
-         xl: "20px",
-       },
-    }}>
-      {title}
-    </Typography>
-  </CardContent>
-
-  {/* Botones */}
-  <CardActions
-    sx={{
-      display: "flex",
-      justifyContent: "space-between",
-      px: 2,
-      pb: 2,
-    }}
-  >
-    <Button
-      variant="contained"
-      color="error"
-      startIcon={<Borrar />}
-      onClick={() => onDelete(id)}
       sx={{
-        borderRadius: 2,
-        textTransform: "none",
-        backgroundColor: "#ff5c5c",
-        color: "#ffffff",
+        width: 350,
+        borderRadius: 3,
+        boxShadow: "0px 4px 8px rgba(255, 255, 255, 0.1)", // Sombra sutil en gris/blanco
+        transition: "transform 0.3s ease, box-shadow 0.3s ease", // Animación suave en hover
+        backgroundColor: "#1e1e1e", // Fondo oscuro para la card
         "&:hover": {
-          backgroundColor: "#e53935",
+          boxShadow: "0px 8px 16px rgba(255, 255, 255, 0.2)", // Sombra más intensa en hover
+          transform: "scale(1.03)", // Efecto de zoom sutil
         },
-            fontWeight: "400",
-            fontSize: {
-              xs: "14px",
-              md: "16px",
-              xl: "18px",
-            },
-            gap:{
-              xs: "5px",
-              md: "10px",
-              xl: "15px",
-            }
+        overflow: "hidden",
       }}
     >
-      Borrar
-    </Button>
+      {/* Imagen */}
+      <CardMedia
+        component="img"
+        height="220"
+        image={img}
+        alt={title}
+        sx={{
+          objectFit: "cover",
+          objectPosition: "top",
+          borderBottom: "1px solid rgba(255, 255, 255, 0.1)", // Línea separadora sutil
+        }}
+      />
 
-    <Button
-      variant="contained"
-      color="primary"
-      startIcon={<Editar />}
-      onClick={handleOpen}
-      sx={{
-        borderRadius: 2,
-        backgroundColor: "#2271D1",
-        color: "#ffffff",
-        "&:hover": {
-          backgroundColor: "#1a5bb5",
-        },
-        textTransform: "none",
-            fontWeight: "400",
-            fontSize: {
-              xs: "14px",
-              md: "16px",
-              xl: "18px",
-            },
-            gap:{
-              xs: "5px",
-              md: "10px",
-              xl: "15px",
-            }
-      }}
-    >
-      Editar
-    </Button>
-  </CardActions>
+      <Box sx={{display:"flex", justifyContent:"space-between"}}>
+        {/* Contenido */}
+        <CardContent sx={{ padding: "10px" }}>
+          <Typography
+            variant="p"
+            fontWeight="bold"
+            textAlign="center"
+            color="#ffffff"
+            sx={{
+              fontWeight: "400",
+              fontSize: {
+                xs: "16px",
+                md: "18px",
+                xl: "20px",
+              },
+            }}
+          >
+            {title}
+          </Typography>
+        </CardContent>
 
+        {/* Botones */}
+        <CardActions
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Button
+            variant="contained"
+            color="error"
+            onClick={() => onDelete(id)}
+            sx={{
+              borderRadius: 2,
+              textTransform: "none",
+              backgroundColor: "transparent",
+              color: "#ffffff",
+              padding: "6px 3px !important",
+              minWidth:"30px",
+              width:"35px",
+              "&:hover": {
+                backgroundColor: "gray",
+              },
+              fontWeight: "400",
+              fontSize: {
+                xs: "14px",
+                md: "16px",
+                xl: "18px",
+              },
+              gap: {
+                xs: "5px",
+                md: "10px",
+                xl: "15px",
+              },
+            }}
+          >
+            <Borrar />
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleOpen}
+            sx={{
+              display:"flex",
+              justifyContent:"center",
+              padding: "6px 3px !important",
+              minWidth:"30px",
+              width:"35px",
+              borderRadius: 2,
+              backgroundColor: "transparent",
+              color: "#ffffff",
+              "&:hover": {
+                backgroundColor: "#ee9f0c",
+              },
+              textTransform: "none",
+              fontWeight: "400",
+              fontSize: {
+                xs: "14px",
+                md: "16px",
+                xl: "18px",
+              },
+              gap: {
+                xs: "5px",
+                md: "10px",
+                xl: "15px",
+              },
+            }}
+          >
+            <Editar />
+          </Button>
+        </CardActions>
+      </Box>
 
       {/* Modal de edición */}
-      <Modal open={open} onClose={handleClose} aria-labelledby="modal-modal-title" aria-describedby="modal-modal-description">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
         <Box sx={modalStyle}>
-          <Typography variant="h5" gutterBottom sx={{color: "black", textAlign: "center", marginBottom: "25px"}}>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{ color: "black", textAlign: "center", marginBottom: "25px" }}
+          >
             Editar Imagen
           </Typography>
           <form onSubmit={handleSubmit}>
             <Box mb={2}>
-              <TextField fullWidth label="Título" variant="outlined" name="Title" value={formData.Title} onChange={handleInputChange} placeholder="Ingresa un título para la imagen" />
+              <TextField
+                fullWidth
+                label="Título"
+                variant="outlined"
+                name="Title"
+                value={formData.Title}
+                onChange={handleInputChange}
+                placeholder="Ingresa un título para la imagen"
+              />
             </Box>
 
             <Box mb={2}>
-              <input type="file" name="file" accept="image/*" onChange={handleInputChange} style={{ display: "none" }} id={`file-upload-${id}`} />
+              <input
+                type="file"
+                name="file"
+                accept="image/*"
+                onChange={handleInputChange}
+                style={{ display: "none" }}
+                id={`file-upload-${id}`}
+              />
               <label htmlFor={`file-upload-${id}`}>
                 <Button variant="contained" component="span" fullWidth>
                   Seleccionar Nueva Imagen
@@ -187,12 +232,21 @@ export const Card = ({ img, title, id, onDelete, onEdit }) => {
 
             {previewUrl && (
               <Box mb={2} sx={{ display: "flex", justifyContent: "center" }}>
-                <img src={previewUrl} alt="Preview" style={{ width: "100%", maxHeight: "200px", objectFit: "contain", borderRadius: "8px" }} />
+                <img
+                  src={previewUrl}
+                  alt="Preview"
+                  style={{
+                    width: "100%",
+                    maxHeight: "200px",
+                    objectFit: "contain",
+                    borderRadius: "8px",
+                  }}
+                />
               </Box>
             )}
 
             <Box sx={{ display: "flex", gap: 2, justifyContent: "flex-end" }}>
-              <Button variant="outlined" onClick={handleClose} >
+              <Button variant="outlined" onClick={handleClose}>
                 Cancelar
               </Button>
               <Button variant="contained" type="submit" color="primary">
